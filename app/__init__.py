2d92760f3524e16107b6cdcd71534ff5dd4fbc95
Rafael Martins <rafaeldcmartins@gmail.com>
2024-04-22 14:24:52 -0300

    config environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
 
-from app.models import init_defaults
+from app.models import db, init_defaults
+
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='postgresql://postgres:123456@localhost:5432/igrejaconectada',
+        SECRET_KEY=os.getenv('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
     )
 
     if test_config is None:
@@ -30,8 +35,12 @@ def create_app(test_config=None):
     from . import announcements
     app.register_blueprint(announcements.bp)
 
-    @app.before_first_request
-    def create_tables():
-        init_defaults()
+    dev_mode = os.getenv('DEBUG')
+
+    if dev_mode:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+            init_defaults()
 
     return app
